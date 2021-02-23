4f7c08998b4583a92764eac6d1e5619ac4ed07eb
Michael J Cipriano <mcipriano@gmail.com>
2021-02-23 15:06:11 -0500

    removed unessecery

--- daoc/form.js
+++ daoc/form.js
@@ -11,8 +11,4 @@ else {
 
 function fillForm() {
     $('input[name="data-v-44091682"]').val('Robert');
-    $('input[name="SURNAME"]').val('Gravelle');
-    $('input[name="EMAIL"]').val('rgconsulting@robgravelle.com');
-    $('input[type="radio"][name="REF"][value="Y"]').attr('checked', 'checked')
-    $('input[name="REF_NAME"]').val('John Bush');
 }
